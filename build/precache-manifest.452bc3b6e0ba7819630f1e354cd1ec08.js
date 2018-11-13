56faf5db4cd875160b9a326841c6a85d1b041ef7
karatehcops <contact@lazers.tv>
2018-11-13 17:11:54 -0500

    Add build folder.

---
+++ build/precache-manifest.452bc3b6e0ba7819630f1e354cd1ec08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "18cd8417d552d86e05f8",
+    "url": "/static/js/main.18cd8417.chunk.js"
+  },
+  {
+    "revision": "479000602b99244b9fde",
+    "url": "/static/js/1.47900060.chunk.js"
+  },
+  {
+    "revision": "18cd8417d552d86e05f8",
+    "url": "/static/css/main.f51eb1a3.chunk.css"
+  },
+  {
+    "revision": "ec213592e2bbdf3672872ce3fcd15c4f",
+    "url": "/index.html"
+  }
+];
