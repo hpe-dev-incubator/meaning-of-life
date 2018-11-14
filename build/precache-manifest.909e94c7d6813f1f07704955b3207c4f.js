e468752f112f7e647e4492c20da35417b85d7a21
karatehcops <contact@lazers.tv>
2018-11-14 13:08:09 -0500

    Add latest build files.

---
+++ build/precache-manifest.909e94c7d6813f1f07704955b3207c4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f60074ca682bb8ef639a",
+    "url": "/static/js/main.f60074ca.chunk.js"
+  },
+  {
+    "revision": "479000602b99244b9fde",
+    "url": "/static/js/1.47900060.chunk.js"
+  },
+  {
+    "revision": "f60074ca682bb8ef639a",
+    "url": "/static/css/main.5a5394ea.chunk.css"
+  },
+  {
+    "revision": "af316461ab6b218d28fe4e5e6bd68688",
+    "url": "/index.html"
+  }
+];
